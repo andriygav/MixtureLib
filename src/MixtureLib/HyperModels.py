4ad3bdaf7b1165f85ae1cbe1bc563352f5716b36
andriygav <andriy.graboviy@mail.ru>
2020-03-21 04:22:17 +0300

    try fix yml

--- src/MixtureLib/HyperModels.py
+++ src/MixtureLib/HyperModels.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
--- tests/test_HyperModels.py
+++ tests/test_HyperModels.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import torch
 
 from MixtureLib.Regularizers import Regularizers
